8ce28a940c5adc83e2ff1efe5b40c132c47bc5f9
Rodrigo Rimbau <rodrigorimbau94@gmail.com>
2023-05-27 03:47:40 -0300

    fx2

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-const PORT = process.env.PORT || 3001
-const DB_USER = process.env.DB_USER || "root"
-const DB_PASSWORD = process.env.DB_PASSWORD || ""
-const DB_HOST = process.env.DB_HOST || "localhost"
+const PORT = process.env.PORT
+const DB_USER = process.env.DB_USER 
+const DB_PASSWORD = process.env.DB_PASSWORD 
+const DB_HOST = process.env.DB_HOST 
 const BD_NAME = process.env.BD_NAME || "serviA"
-const BD_PORT = process.env.BD_PORT || 3306
+const BD_PORT = process.env.BD_PORT 
 const SECRET = '3306'
 const dialect = 'mysql'
 
